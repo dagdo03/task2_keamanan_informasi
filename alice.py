f26841c68af1a7b04a79ea7801422ab536634956
ihsanwidagdo <ihsan.widagdo@gmail.com>
2024-10-30 21:44:01 +0700

    feat: fix bug

--- alice.py
+++ alice.py
@@ -15,35 +15,34 @@ def alice():
     print("Let's Talks with bob")
     while(True):
         plain_text = input("Masukkan Plaintext: ")
-        if(plain_text != "exit"):
-            print("Plaintext: " + plain_text)
-            print("Key: " + key_des)
-            key_des = hex2bin(key_des)
-            key_des = permute(key_des, keyp, 56)
-            # Splitting
-            left = key_des[0:28]    # rkb for RoundKeys in binary
-            right = key_des[28:56]  # rk for RoundKeys in hexadecimal
-            rkb = []
-            rk = []
+        if(plain_text == "exit"):
+            break
+            server_socket.close()
+        print("Plaintext: " + plain_text)
+        print("Key: " + key_des)
+        key_des = hex2bin(key_des)
+        key_des = permute(key_des, keyp, 56)
+        # Splitting
+        left = key_des[0:28]    # rkb for RoundKeys in binary
+        right = key_des[28:56]  # rk for RoundKeys in hexadecimal
+        rkb = []
+        rk = []
+        for i in range(0, 16):
+            # Shifting the bits by nth shifts by checking from shift table
+            left = shift_left(left, shift_table[i])
+            right = shift_left(right, shift_table[i])
+            # Combination of left and right string
+            combine_str = left + right
+            # Compression of key_des from 56 to 48 bits
+            round_key = permute(combine_str, key_comp, 48)
+            rkb.append(round_key)
+            rk.append(bin2hex(round_key))
             
-            for i in range(0, 16):
-                # Shifting the bits by nth shifts by checking from shift table
-                left = shift_left(left, shift_table[i])
-                right = shift_left(right, shift_table[i])
-                # Combination of left and right string
-                combine_str = left + right
-                # Compression of key_des from 56 to 48 bits
-                round_key = permute(combine_str, key_comp, 48)
-                rkb.append(round_key)
-                rk.append(bin2hex(round_key))
-            
-            print("Encryption")
-            ct, added_char = ecb_encrypt(plain_text, rkb, rk)
-            original_ct = bin2hex(ct) + added_char
-            print("Cipher Text : ", original_ct)
-            client_socket.send(original_ct.encode())
-        else:
-            return
+        print("Encryption")
+        ct, added_char = ecb_encrypt(plain_text, rkb, rk)
+        original_ct = bin2hex(ct) + added_char
+        print("Cipher Text : ", original_ct)
+        client_socket.send(original_ct.encode())
 
 if __name__ == '__main__':
     alice()
